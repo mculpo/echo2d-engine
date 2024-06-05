13dd89e596bf19b66052d77bb69aaac7173ba109
Mateus R. Culpo <mateusculpo@gmail.com>
2024-06-05 08:13:19 -0300

    feat: implementing RenderHeathBarSystem

--- echo2d-engine/src/Systems/RenderHealthBarSystem.h
+++ echo2d-engine/src/Systems/RenderHealthBarSystem.h
@@ -10,23 +10,81 @@ class RenderHealthBarSystem : public System {
 public:
 	RenderHealthBarSystem() {
 		RequireComponent<TransformComponent>();
-		RequireComponent<SpriteComponent>(); 
+		RequireComponent<SpriteComponent>();
 		RequireComponent<HealthComponent>();
 	}
 
 	void Update(SDL_Renderer* pRenderer, std::unique_ptr<AssetStore>& pAssetStore, SDL_Rect& pCamera) {
+		// Pré-calcular a fonte uma vez para reutilização
+		TTF_Font* font = pAssetStore->GetFont("pico8-font-10");
+
+		// Criar uma SDL_Surface temporária para a renderização do texto fora do loop
+		SDL_Surface* surface = nullptr;
+		// Criar uma SDL_Texture temporária para a renderização do texto fora do loop
+		SDL_Texture* texture = nullptr;
+
 		for (auto entity : GetSystemEntities()) {
+
 			const auto transform = entity.GetComponent<TransformComponent>();
 			const auto sprite = entity.GetComponent<SpriteComponent>();
 			const auto health = entity.GetComponent<HealthComponent>();
 
-			SDL_Color currentColor = SDL_COLOR_GREEN;
-			
-			int rectPositionX = transform.position.x - sprite.srcRect.x/2;
-			int rectPositionY = transform.position.y - sprite.srcRect.y/2;
-			int rectWidth = sprite.srcRect.x;
-			int rectHeight = 3;
+			SDL_Color currentColor = GetHealthColor(health.healthPercentage);
 
+			int rectPositionX = transform.position.x - pCamera.x;
+			int rectPositionY = transform.position.y - 7 - pCamera.y;
+			int rectWidth = sprite.srcRect.w * transform.scale.x;
+			int rectHeight = 5;
+
+			SDL_Rect rect = {
+				rectPositionX,
+				rectPositionY,
+				rectWidth * (health.healthPercentage / 100.0),
+				rectHeight
+			};
+
+			SDL_SetRenderDrawColor(pRenderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
+			SDL_RenderFillRect(pRenderer, &rect);
+
+			std::string healthText = std::to_string(health.healthPercentage);
+
+			surface = TTF_RenderText_Blended(font, healthText.c_str(), currentColor);
+			texture = SDL_CreateTextureFromSurface(pRenderer, surface);
+
+
+			int labelWidth = 0;
+			int labelHeight = 0;
+
+			SDL_QueryTexture(texture, NULL, NULL, &labelWidth, &labelHeight);
+
+			SDL_Rect dstRect = {
+				rectPositionX,
+				rectPositionY - 14,
+				labelWidth,
+				labelHeight
+			};
+
+			SDL_RenderCopy(pRenderer, texture, NULL, &dstRect);
+
+		}
+		if (surface)
+			SDL_FreeSurface(surface);
+		if (texture)
+			SDL_DestroyTexture(texture);
+	}
+private:
+
+
+
+	SDL_Color GetHealthColor(int healthPercentage) {
+		if (healthPercentage > 70) {
+			return SDL_COLOR_GREEN; // Verde
+		}
+		else if (healthPercentage > 30) {
+			return SDL_COLOR_YELLOW; // Amarelo
+		}
+		else {
+			return SDL_COLOR_RED; // Vermelho
 		}
 	}
 };
